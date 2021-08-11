338e2a544ed86c052c9867c5b75882f587da4e6d
acmo0 <acmo0.tty@free.fr>
2021-08-11 10:14:49 +0200

    Update commit

--- app/src/main/java/com/acmo0/youtubedownloader/DownloaderWorker.java
+++ app/src/main/java/com/acmo0/youtubedownloader/DownloaderWorker.java
@@ -1,7 +1,19 @@
 package com.acmo0.youtubedownloader;
-import android.content.Context;
+import static android.content.Context.NOTIFICATION_SERVICE;
+import static android.os.Build.ID;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.util.Config;
+
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.NotificationManagerCompat.*;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
@@ -29,39 +41,68 @@ public class DownloaderWorker extends Worker {
         String videoUrl = getInputData().getString(VIDEO_URL);
         File downloadDirectory = new File(directory);
         boolean success = true;
+        makeNotificationChannel("CHANNEL", "Download status", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(getApplicationContext());
+        NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL");
 
         if(!downloadDirectory.exists()){
             success = downloadDirectory.mkdirs();
             if(!success){
-                notify("Download failed");
+                notifyFail(mNotificationManager, notifBuilder);
                 return Result.failure();
             }
         }
+
+
 
         Python py = Python.getInstance();
         PyObject pyf = py.getModule("downloader");
         PyObject sys = py.getModule("sys");
         PyObject downloader;
         //try {
+
+        notifyDownloading(mNotificationManager, notifBuilder, "Downloading");
         downloader = pyf.callAttr("download", videoUrl, format, directory, maxQuality);
         /*}catch (Error e) {
             notify("Error while downloading");
             return Result.failure();
         }*/
-        notify(downloader.toString());
         if(downloader.toBoolean() == false){
+            notifyFail(mNotificationManager, notifBuilder);
             return Result.failure();
         }
         else{
-            notify("Download successes");
+            notifySuccess(mNotificationManager, notifBuilder);
             return Result.success();
         }
     }
-    void notify(String text){
-        NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(getApplicationContext(), "downloader-yt")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+    void notifyDownloading(NotificationManagerCompat mNotificationManager, NotificationCompat.Builder notifBuilder, String text){
+
+        notifBuilder
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Youtube Downloader")
                 .setContentText(text)
+                .setProgress(0, 0, true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        mNotificationManager.notify(1, notifBuilder.build());
+    }
+    void notifyFail(NotificationManagerCompat mNotificationManager, NotificationCompat.Builder notifBuilder){
+        notifBuilder.setContentText("Sorry, error while downloading...").setProgress(0,0,false);
+        mNotificationManager.notify(1, notifBuilder.build());
+    }
+    void notifySuccess(NotificationManagerCompat mNotificationManager, NotificationCompat.Builder notifBuilder){
+        notifBuilder.setContentText("Download finished !").setProgress(0,0,false);
+        mNotificationManager.notify(1, notifBuilder.build());
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    void makeNotificationChannel(String id, String name, int importance)
+    {
+        NotificationChannel channel = new NotificationChannel(id, name, importance);
+
+        NotificationManager notificationManager =
+                (NotificationManager)getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
+
+        assert notificationManager != null;
+        notificationManager.createNotificationChannel(channel);
     }
 }
