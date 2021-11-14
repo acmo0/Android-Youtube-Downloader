513b5ec16128b6ddc45a080f1304c9fb6302ae4b
acmo0 <acmo0.tty@free.fr>
2021-11-14 21:29:39 +0100

    v1.1 commit

--- app/src/main/java/com/acmo0/youtubedownloader/DownloaderWorker.java
+++ app/src/main/java/com/acmo0/youtubedownloader/DownloaderWorker.java
@@ -6,11 +6,14 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
@@ -23,6 +26,8 @@ import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 
 import java.io.File;
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.annotations.NonNull;
 
@@ -32,6 +37,11 @@ public class DownloaderWorker extends Worker {
     public static final String DIRECTORY = "DIRECTORY";
     public static final String MAX_QUALITY = "MAX_QUALITY";
     public static final String VIDEO_URL = "VIDEO_URL";
+    public static PyObject downloader;
+    NotificationManagerCompat mNotificationManager;
+    NotificationCompat.Builder notifBuilder;
+    PendingIntent activityPendingIntent;
+
     public DownloaderWorker(@NonNull Context context, @NonNull WorkerParameters params){
         super(context, params);
     }
@@ -43,9 +53,10 @@ public class DownloaderWorker extends Worker {
         String videoUrl = getInputData().getString(VIDEO_URL);
         File downloadDirectory = new File(directory);
         boolean success;
+
         makeNotificationChannel("CHANNEL", "Download status", NotificationManager.IMPORTANCE_DEFAULT);
-        NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(getApplicationContext());
-        NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL");
+        mNotificationManager = NotificationManagerCompat.from(getApplicationContext());
+        notifBuilder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL");
 
 
         Intent activityIntent = new Intent(getApplicationContext(), MainActivity.class);
@@ -55,7 +66,7 @@ public class DownloaderWorker extends Worker {
         activityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
         stackBuilder.addNextIntentWithParentStack(activityIntent);
-        PendingIntent activityPendingIntent = PendingIntent.getActivity(getApplicationContext(),0,activityIntent, 0);
+        activityPendingIntent = PendingIntent.getActivity(getApplicationContext(),0,activityIntent, 0);
 
         if(!downloadDirectory.exists()){
             success = downloadDirectory.mkdirs();
@@ -67,27 +78,60 @@ public class DownloaderWorker extends Worker {
 
         Python py = Python.getInstance();
         PyObject pyf = py.getModule("downloader");
-        PyObject downloader;
+
 
 
         notifyDownloading(mNotificationManager, notifBuilder, getApplicationContext().getResources().getString(R.string.text_notif_downloading), activityPendingIntent);
-        downloader = pyf.callAttr("download", videoUrl, format, directory, maxQuality, Environment.getExternalStorageDirectory().getPath()+"/Android/data/com.acmo0.youtubedownloader");
-
-        if(downloader.toBoolean() == false){
-            notifyFail(mNotificationManager, notifBuilder, activityPendingIntent);
-            return Result.failure();
+        downloader = pyf.callAttr("downloader", videoUrl, format, directory, maxQuality, Environment.getExternalStorageDirectory().getPath()+"/Android/data/com.acmo0.youtubedownloader");
+        PyObject download = downloader.callAttr("download");
+        System.out.println("Started downloading");
+        while (downloader.callAttr("state").toBoolean()!=false){
+            notifBuilder.setContentText(downloader.get("status").toString());
+            mNotificationManager.notify(1,notifBuilder.build());
+            if(downloader.get("fail").toBoolean() == true){
+                mNotificationManager.cancelAll();
+                notifyFail(mNotificationManager, notifBuilder, activityPendingIntent);
+                return Result.failure();
+            }
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        else{
+        if(downloader.get("fail").toBoolean() == false) {
             notifySuccess(mNotificationManager, notifBuilder, activityPendingIntent);
             return Result.success();
         }
+        mNotificationManager.cancelAll();
+        notifyFail(mNotificationManager, notifBuilder, activityPendingIntent);
+        return Result.failure();
+    }
+
+    @Override
+    public void onStopped(){
+        System.out.println("STOOOOOOP");
+        downloader.put("stop",true);
+        try {
+            TimeUnit.MILLISECONDS.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mNotificationManager.cancelAll();
+        notifyStop(mNotificationManager, notifBuilder, activityPendingIntent);
+        super.onStopped();
     }
     void notifyDownloading(NotificationManagerCompat mNotificationManager, NotificationCompat.Builder notifBuilder, String text, PendingIntent mPendingIntent){
+        Intent stopIntent = new Intent(getApplicationContext(), StopDownloadBroadcast.class);
+        PendingIntent stopPIntent = PendingIntent.getBroadcast(getApplicationContext(),1,stopIntent,0);
         notifBuilder
+                .setContentIntent(mPendingIntent)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Video Downloader")
                 .setContentText(text)
                 .setProgress(0, 0, true)
+                .setOnlyAlertOnce(true)
+                .addAction(R.drawable.ic_background,"Cancel",stopPIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         mNotificationManager.notify(1, notifBuilder.build());
     }
@@ -97,6 +141,10 @@ public class DownloaderWorker extends Worker {
     }
     void notifySuccess(NotificationManagerCompat mNotificationManager, NotificationCompat.Builder notifBuilder, PendingIntent mPendingIntent){
         notifBuilder.setContentIntent(mPendingIntent).setSmallIcon(R.mipmap.ic_launcher).setContentText(getApplicationContext().getResources().getString(R.string.text_notif_download_finished)).setProgress(0,0,false);
+        mNotificationManager.notify(1, notifBuilder.build());
+    }
+    void notifyStop(NotificationManagerCompat mNotificationManager, NotificationCompat.Builder notifBuilder, PendingIntent mPendingIntent){
+        notifBuilder.setContentIntent(mPendingIntent).setSmallIcon(R.mipmap.ic_launcher).setContentText(getApplicationContext().getResources().getString(R.string.text_notif_stop)).setProgress(0,0,false);
         mNotificationManager.notify(1, notifBuilder.build());
     }
     @RequiresApi(api = Build.VERSION_CODES.O)
--- app/src/main/python/downloader.py
+++ app/src/main/python/downloader.py
@@ -1,65 +1,111 @@
 from __future__ import unicode_literals
 import youtube_dl
 import os.path
+import os
 import time
+from threading import *
+from multiprocessing import Process
+import sys
 
-def getTime():
-    time_stamp = time.localtime()
-    return str(time_stamp.tm_mday)+"/"+str(time_stamp.tm_mon)+"/"+str(time_stamp.tm_year)+"::"+str(time_stamp.tm_hour)+":"+str(time_stamp.tm_min)+":"+str(time_stamp.tm_sec)
+class downloader:
+    def __init__(self,url, dl_format, directory, max_best_quality, log_path):
+        self.url = url
+        self.dl_format = dl_format
+        self.directory = directory
+        self.max_best_quality = max_best_quality
+        self.log_path = log_path
+        self.fail = False
+        self.stop=False
+        self.status = 'Stopped'
+        print("initialise")
+        
+    def getTime(self):
+        time_stamp = time.localtime()
+        return str(time_stamp.tm_mday)+"/"+str(time_stamp.tm_mon)+"/"+str(time_stamp.tm_year)+"::"+str(time_stamp.tm_hour)+":"+str(time_stamp.tm_min)+":"+str(time_stamp.tm_sec)
 
-def writeLog(message,logfile, filename = "/yt.log.txt"):
-    message = getTime()+"="+message+"\n"
-    
-    if not os.path.exists(logfile):
+    def writeLog(self,message,logfile, filename = "/yt.log.txt"):
+        message = self.getTime()+"="+message+"\n"
+        
+        if not os.path.exists(logfile):
+            try:
+                os.mkdir(logfile)
+                with open(logfile+filename, 'w+') as lf:
+                    lf.write(message)
+            except Exception as e:
+                print(e)
+        else:
+            try:
+                with open(logfile+filename, "a") as lf:
+                    lf.write(message)
+            except Exception as e:
+                print(e)
+
+    def download(self):
+        self.status='Preparing'
+        self.process = Thread(target=self.downloadAction,args=(self.directory,self.dl_format, self.max_best_quality,self.url))
+        
+        print('init')
+        if os.path.exists(self.log_path+"/yt.log.txt"):
+            os.remove(self.log_path+"/yt.log.txt")
         try:
-            os.mkdir(logfile)
-            with open(logfile+filename, 'w+') as lf:
-                lf.write(message)
-        except Exception as e:
-            print(e)
-    else:
-        try:
-            with open(logfile+filename, "a") as lf:
-                lf.write(message)
-        except Exception as e:
-            print(e)
-
-
-def downloadAction(url, dl_format, directory, max_best_quality):
-    ydl_opts = {}
-    if directory[-1] != "/":
-        directory+="/"
-    if dl_format == "mp4":
-        ydl_opts = {
-            'format': 'bestvideo[height<='+max_best_quality+']+bestaudio/best[height<='+max_best_quality+']',
-            'outtmpl': directory+"%(title)s.%(ext)s"
-        }
-    elif dl_format == "m4a":
-        ydl_opts = {
-            'format': 'bestaudio[ext=m4a]/best[ext=m4a]',
-            'outtmpl': directory+"%(title)s.%(ext)s"
-        }
-    print(ydl_opts)
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-def download(url, dl_format, directory, max_best_quality, log_path):
-    if os.path.exists(log_path+"/yt.log.txt"):
-        os.remove(log_path+"/yt.log.txt")
-    try:
-
-        downloadAction(url, dl_format, directory, max_best_quality)
-        return True
-    except Exception as e:
-        if "not a valid URL" in str(e):
-            writeLog("ERROR :"+str(e), log_path)
-            return False
-        time.sleep(10)
-        writeLog("PY WARNING : first exception :"+str(e)+", wait 10s and retry", log_path)
-        try:
-            downloadAction(url, dl_format, directory, mac_best_quality)
+            self.process.start()
+            print('start')
             return True
         except Exception as e:
-            writeLog("PY ERROR :"+str(e), log_path)
-            print(e)
+            print('Erreur')
+            sys.stdout.write(str(e))
+            if "not a valid URL" in str(e):
+                self.writeLog("ERROR :"+str(e), self.log_path)
+                return False
+            time.sleep(10)
+            self.writeLog("PY WARNING : first exception :"+str(e)+", wait 10s and retry", self.log_path)
             return False
+    def downloadAction(self,directory,dl_format,max_best_quality,url):
+        sys.stdout.write("process started")
+        ydl_opts = {}
+        if directory[-1] != "/":    
+            directory+="/"
+        if dl_format == "mp4":
+            ydl_opts = {
+                'format': 'bestvideo[height<='+max_best_quality+']+bestaudio/best[height<='+max_best_quality+']',
+                'outtmpl': directory+"%(title)s.%(ext)s",
+                'progress_hooks': [self.my_hook],
+            }
+        elif dl_format == "m4a":
+            ydl_opts = {
+                'format': 'bestaudio[ext=m4a]/best[ext=m4a]',
+                'outtmpl': directory+"%(title)s.%(ext)s",
+                'progress_hooks': [self.my_hook],
+            }
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+        except ValueError:
+            pass
+        except Exception as e:
+            self.fail = True
+        sys.stdout.write("process endeed")
+            
+    def my_hook(self,d):
+        if d['status'] == 'finished':
+            file_tuple = os.path.split(os.path.abspath(d['filename']))
+            print("Done downloading {}".format(file_tuple[1]))
+            self.status='convert'
+            if self.stop==True:
+                self.process.terminate()
+        if d['status'] == 'downloading':
+            print(d['filename'], d['_percent_str'], d['_eta_str'])
+            self.status=d['filename'].split("/")[-1]+' : '+d['_percent_str']
+            print(self.stop)
+            if self.stop==True:
+                self.status='Stopped'
+                print(self.process)
+                raise ValueError('Stopping process')
+            
+    def state(self):
+        return self.process.is_alive()
+    def stop(self):
+        sys.stdout.write('Stopping')
+        self.stop=True
+
+
